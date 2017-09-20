a4bf79938abecb5d926caae82c4d66402d2691bb
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-09-20 22:06:22 +0200

    Added ajax method.

--- EmployeeService/Scripts/main.js
+++ EmployeeService/Scripts/main.js
@@ -1,1 +1,24 @@
-﻿
+﻿// Click method for the modal to open
+$("#btnRegister").on("click", function () {
+    $.ajax({
+        url: "/api/account/register",
+        method: "POST",
+        data: {
+            email: $("#txtEmail").val(),
+            password: $("#txtPassword").val(),
+            confirmPassword: $("#txtConfirmPassword").val()
+        },
+        success: function () {
+            $("#successModal").modal("show");
+        },
+        error: function (jqXHR) {
+            $("#divErrorText").text(jqXHR.responseText);
+            $("#divError").show("fade");
+        }
+    });
+});
+
+// Close alert window
+$("#linkClose").on("click", function () {
+    $("#divError").hide("show");
+});
