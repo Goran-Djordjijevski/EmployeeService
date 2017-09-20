72196431ec5ccabf89ebbd30a232fb34762d6d9a
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-09-20 20:00:19 +0200

    Added new EmployeeController class and added new Entity Models for Sql Server.

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeService.EntityModels;
+
+namespace EmployeeService.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDbContext db = new EmployeeDbContext())
+            {
+                List<Employee> listOfEmployees = db.Employees.ToList();
+
+                return listOfEmployees;
+            }
+        }
+    }
+}
